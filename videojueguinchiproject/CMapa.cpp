d0ed945d680906ee21ce8af5a482ef416e00ec62
APerezG95 <adr.perez95@gmail.com>
2016-04-30 10:18:45 +0200

    Sincronizacion
    Me daba error al sincronizar con la herramienta. He bajado un zip con
    todo y lo he cambiado en mi ordena a capón

---
+++ videojueguinchiproject/CMapa.cpp
@@ -0,0 +1,41 @@
+/*    .-----------------------------------------------------------------.    */
+/*   /  .-.                                                         .-.  \   */
+/*  |  /   \          Proyecto: Videojuego Informática             /   \  |  */
+/*  | |\_.  |                                                     |    /| |  */
+/*  |\|  | /|         Autor: Mario Pedraza Esteban                |\  | |/|  */
+/*  | `---' |                                                     | `---' |  */
+/*  |       |         Fecha Última Modificación: 22/04/2016       |       |  */
+/*  |       |-----------------------------------------------------|       |  */
+/*  \       |                                                     |       /  */
+/*   \     /                                                       \     /   */
+/*    `---'                                                         `---'    */
+
+#include "CMapa.h"
+
+CMapa::CMapa(int sizex, int sizey) //En principio inicializamos todas las casillas a NADA
+	:m_iSizex(sizex),
+	m_iSizey(sizey)
+{
+	m_eBoard = new m_eEnte*[sizey];		//Ojo puede estar mal esto
+
+	for (int i = 0; i<sizey; i++) 
+	{
+		m_eBoard[i] = new m_eEnte[sizex];
+	}
+
+	for (int i = 0; i<sizex; i++) 
+	{
+		for (int j = 0; j<sizey; j++) 
+		{
+			m_eBoard[i][j] = NADA;
+		}
+	}
+}
+
+CMapa::~CMapa()
+{
+	for (int i = 0; i<m_iSizey; i++) {
+		delete[] m_eBoard[i];
+	}
+	delete[] m_eBoard;
+}
--- videojueguinchiproject/CPersonaje.cpp
+++ videojueguinchiproject/CPersonaje.cpp
@@ -1,37 +1,60 @@
+/*    .-----------------------------------------------------------------.    */
+/*   /  .-.                                                         .-.  \   */
+/*  |  /   \          Proyecto: Videojuego Informática             /   \  |  */
+/*  | |\_.  |                                                     |    /| |  */
+/*  |\|  | /|         Autor: Mario Pedraza Esteban                |\  | |/|  */
+/*  | `---' |                                                     | `---' |  */
+/*  |       |         Fecha Última Modificación: 22/04/2016       |       |  */
+/*  |       |-----------------------------------------------------|       |  */
+/*  \       |                                                     |       /  */
+/*   \     /                                                       \     /   */
+/*    `---'                                                         `---'    */
+
 #include "CPersonaje.h"
 
 int CPersonaje::Atacar(int consumo, bool tipodaño, int proporcion)
 {
-	if (tipodaño == 0)
-		return m_iAtq_fis;
+	if (tipodaño == 0)					//Daño físico
+		return m_iAtq_fis;				//Devuelve el daño crudo
 
 	else
 	{
-		m_iAguante -= consumo;
-		return m_iAtq_hab*proporcion;
+		m_iAguante -= consumo;			//Daño de habilidad
+		return m_iAtq_hab*proporcion;	//Devuelve el daño con la habilidad proporcionalmente
 	}
 }
 
 int CPersonaje::Defender(int dmg, bool tipodaño)
 {
-	if (tipodaño == 0)
-		return (dmg-dmg*m_iDef_fis);
+	if (tipodaño == 0)					//Daño físico
+		return (dmg - dmg*m_iDef_fis);	//Reducción proporcional con defensa física
 
 	else
-		return (dmg - dmg*m_iDef_hab);
+		return (dmg - dmg*m_iDef_hab);	//Daño de habil, idem con la defensa de habil.
 
 }
 
 bool CPersonaje::Moverse(CPosicion input)
 {
-	int pos_max_x = m_iVel + m_Pos.x;
-	int pos_max_y = m_iVel + m_Pos.y;
+	int pos_max_x = m_iVel + m_Pos.x;	//Posición máxima a la que se puede mover en x
+	int pos_max_y = m_iVel + m_Pos.y;	//Posición máxima a la que se puede mover en y
 
 	if ((pos_max_x < input.x) || (pos_max_y < input.y))	//posible sobrecarga de operador <
-		return 0;
+		return 0;						//Error, no se puede mover tan lejos
 
-	m_Pos = input;
-	return 1;
+	m_Pos = input;					
+	return 1;							//Ok, se mueve a esa posición
 
 
 }
+
+void CPersonaje::Añadir_Buff()
+{
+	if (!m_bObjOn)
+	{
+		//Meter aquí la función item para calcular el buff y meterlo al personaje.
+		m_bObjOn = true;
+	}
+}
+
+//Falta la función actualizar y posiblemente el constructor
