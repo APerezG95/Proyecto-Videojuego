fb30f801894aff3eb7d40eb00238ad6d8cbb7166
APerezG95 <adr.perez95@gmail.com>
2016-06-09 12:45:56 +0200

    Update CJefe.cpp

--- videojueguinchiproject/CJefe.cpp
+++ videojueguinchiproject/CJefe.cpp
@@ -2,8 +2,19 @@
 
 
 
-CJefe::CJefe()
+CJefe::CJefe(int a, int b, int c, float d, float e, int f, int g)
 {
+	m_bObjOn = false;
+	m_iAtq_fis = a;
+	m_iAtq_hab = b;
+	m_iSaludMax = m_iSalud = c;
+	m_iDef_fis = d;
+	m_iDef_hab = e;
+	m_iVel = 3;				//constante en todos los enemigos
+	m_iBando = false;
+	m_iNivel = f;
+	m_iAguante = m_iAguanteMax = g;
+	m_bDisp = true;
 }
 
 
