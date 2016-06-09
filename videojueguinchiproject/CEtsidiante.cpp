df8032b73578113e2ca07f8dfc32b22c15d57fc1
APerezG95 <adr.perez95@gmail.com>
2016-06-09 12:28:14 +0200

    Update CEtsidiante.cpp

--- videojueguinchiproject/CEtsidiante.cpp
+++ videojueguinchiproject/CEtsidiante.cpp
@@ -1,9 +1,24 @@
 #include "CEtsidiante.h"
 
 
+void CEtsidiante::Inicializa()
+{
+	m_bObjOn = false;
+	m_iAtq_fis = 20;					//Escala sobre 100
+	m_iAtq_hab = 20;
+	m_iSaludMax = m_iSalud = 600;		//Escala sobre 1000
+	m_iDef_fis = 0.5f;
+	m_iDef_hab = 0.5f;
+	m_iVel = 4;							//Escala sobre 20
+	m_iBando = true;
+	m_iNivel = 1;
+	m_iAguante = m_iAguanteMax = 120;	//Escala sobre 500
+	m_bDisp = true;
+}
 
 CEtsidiante::CEtsidiante()
 {
+	CEtsidiante::Inicializa();
 }
 
 
