e038d0391a8a48234d8fd2b1e7e2041dabf1e976
APerezG95 <adr.perez95@gmail.com>
2016-06-09 12:09:22 +0200

    Update CEmpollon.cpp

--- videojueguinchiproject/CEmpollon.cpp
+++ videojueguinchiproject/CEmpollon.cpp
@@ -1,11 +1,25 @@
 #include "CEmpollon.h"
 
 
+void CEmpollon::Inicializa()
+{
+	m_bObjOn = false;
+	m_iAtq_fis = 40;					//Escala sobre 100
+	m_iAtq_hab = 0;
+	m_iSaludMax = m_iSalud = 300;		//Escala sobre 1000
+	m_iDef_fis = 0.3f;
+	m_iDef_hab = 0.3f;
+	m_iVel = 4;							//Escala sobre 20
+	m_iBando = true;
+	m_iNivel = 1;
+	m_iAguante = m_iAguanteMax = 100;	//Escala sobre 500
+	m_bDisp = true;
+}
 
 CEmpollon::CEmpollon()
 {
+	CEmpollon::Inicializa();
 }
-
 
 CEmpollon::~CEmpollon()
 {
