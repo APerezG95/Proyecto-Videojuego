a460369e7bc48b3b3911832eea288c35b769d6af
APerezG95 <adr.perez95@gmail.com>
2016-06-09 12:39:55 +0200

    Update CHippie.cpp

--- videojueguinchiproject/CHippie.cpp
+++ videojueguinchiproject/CHippie.cpp
@@ -1,9 +1,25 @@
 #include "CHippie.h"
 
 
+void CHippie::Inicializa()
+{
+	m_bObjOn = false;
+	m_iAtq_fis = 10;					//Escala sobre 100
+	m_iAtq_hab = 40;
+	m_iSaludMax = m_iSalud = 400;		//Escala sobre 1000
+	m_iDef_fis = 0.4f;
+	m_iDef_hab = 0.4f;
+	m_iVel = 3;							//Escala sobre 20
+	m_iBando = true;
+	m_iNivel = 1;
+	m_iAguante = m_iAguanteMax = 200;	//Escala sobre 500
+	m_bDisp = true;
+}
+
 
 CHippie::CHippie()
 {
+	CHippie::Inicializa();
 }
 
 
