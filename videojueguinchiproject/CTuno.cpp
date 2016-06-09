65b191cb757c1e4c2cef491f4abee8478180ec32
APerezG95 <adr.perez95@gmail.com>
2016-06-09 12:54:31 +0200

    Update CTuno.cpp

--- videojueguinchiproject/CTuno.cpp
+++ videojueguinchiproject/CTuno.cpp
@@ -1,9 +1,24 @@
 #include "CTuno.h"
 
 
+void CTuno::Inicializa()
+{
+	m_bObjOn = false;
+	m_iAtq_fis = 5;					//Escala sobre 100
+	m_iAtq_hab = 5;
+	m_iSaludMax = m_iSalud = 50;		//Escala sobre 1000
+	m_iDef_fis = 0.1f;
+	m_iDef_hab = 0.1f;
+	m_iVel = 20;							//Escala sobre 20
+	m_iBando = true;
+	m_iNivel = 1;
+	m_iAguante = m_iAguanteMax = 20;	//Escala sobre 500
+	m_bDisp = true;
+}
 
 CTuno::CTuno()
 {
+	CTuno::Inicializa;
 }
 
 
