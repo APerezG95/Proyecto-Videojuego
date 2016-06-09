d1f0e749f7a0774dbebb7445d9bbc0fa911582b5
APerezG95 <adr.perez95@gmail.com>
2016-06-09 11:52:05 +0200

    Update CPersonaje.h

--- videojueguinchiproject/CPersonaje.h
+++ videojueguinchiproject/CPersonaje.h
@@ -1,10 +1,10 @@
 /*    .-----------------------------------------------------------------.    */
 /*   /  .-.                                                         .-.  \   */
-/*  |  /   \          Proyecto: Videojuego Informática             /   \  |  */
+/*  |  /   \          Proyecto: Videojuego InformÃ¡tica             /   \  |  */
 /*  | |\_.  |                                                     |    /| |  */
 /*  |\|  | /|         Autor: Mario Pedraza Esteban                |\  | |/|  */
 /*  | `---' |                                                     | `---' |  */
-/*  |       |         Fecha Última Modificación: 18/05/2016       |       |  */
+/*  |       |         Fecha Ãšltima ModificaciÃ³n: 18/05/2016       |       |  */
 /*  |       |-----------------------------------------------------|       |  */
 /*  \       |                                                     |       /  */
 /*   \     /                                                       \     /   */
@@ -19,29 +19,30 @@
 class CPersonaje: public CEnte
 {
 protected:
-	CItem m_Obj;		//Variable para almacenar las características del objeto
+	CItem m_Obj;		//Variable para almacenar las caracterÃ­sticas del objeto
 	bool m_bObjDisp;	//Variable para saber si tengo un objeto o no. 0 para tengo objeto, 1 para no tengo objeto
-	bool m_bObjOn;		//Variable de comprobación. 0 para buff no aplicado, 1 para buff aplicado
-	int m_iAtq_fis;		//Ataque directo o físico. Estadística base del personaje
+	bool m_bObjOn;		//Variable de comprobaciÃ³n. 0 para buff no aplicado, 1 para buff aplicado
+	int m_iAtq_fis;		//Ataque directo o fÃ­sico. EstadÃ­stica base del personaje
 	int m_iAtq_hab;		//Ataque de habilidad
-	int m_iSaludMax;	//Puntos de daño total que es capaz de soportar
+	int m_iSaludMax;	//Puntos de daÃ±o total que es capaz de soportar
 	int m_iSalud;		//Puntos de salud actuales
-	float m_iDef_fis;	//Valor porcentual de reducción de daño físico recibido
-	float m_iDef_hab;	//Valor porcentual de reducción de daño de habilidad recibido
-	int m_iVel;			//Número de casillas máximo que es capaz de moverse el personaje
+	float m_iDef_fis;	//Valor porcentual de reducciÃ³n de daÃ±o fÃ­sico recibido
+	float m_iDef_hab;	//Valor porcentual de reducciÃ³n de daÃ±o de habilidad recibido
+	int m_iVel;			//NÃºmero de casillas mÃ¡ximo que es capaz de moverse el personaje
 	bool m_iBando;		//aliados=1 enemigos=0                       
-	int m_iNivel;		//Número que representa la cantidad de experiencia recibida y determina sus atributos
+	int m_iNivel;		//NÃºmero que representa la cantidad de experiencia recibida y determina sus atributos
 	int m_iAguante;		//Puntos de aguante actuales
-	int m_iAguanteMax;	//Puntos máximos de aguante, permiten lanzar ataques de habilidad
+	int m_iAguanteMax;	//Puntos mÃ¡ximos de aguante, permiten lanzar ataques de habilidad
 	bool m_bDisp;		//disponibilidad. muerto=0 vivo=1
 
 public:
-	virtual void Inicializa()=0;								//Requiere inicialización explícita en función del tipo
-	virtual int Atacar(int consumo=0, bool tipodaño=0, int daño_base=1);	//Tipodaño 0 para físico y 1 para habilidad. daño_base es el daño de la habilidad. Consumo es para el gasto de Aguante.
-	virtual int Defender(int dmg, bool tipodaño=0);
+	virtual void Inicializa()=0;								//Requiere inicializaciÃ³n explÃ­cita en funciÃ³n del tipo
+	virtual int Atacar(int consumo=0, bool tipodaÃ±o=0, int daÃ±o_base=1);	//TipodaÃ±o 0 para fÃ­sico y 1 para habilidad. daÃ±o_base es el daÃ±o de la habilidad. Consumo es para el gasto de Aguante.
+	virtual int Defender(int dmg, bool tipodaÃ±o=0);
 	void Actualizar();
 	bool Moverse(CPosicion input, CMapa* map);
-	virtual void Añadir_Buff(CBonus bonus);
+	virtual void AÃ±adir_Buff(CBonus bonus);
 	virtual void Recoger_Objeto(CEnte* item);
+	CPersonaje();
 
 };
