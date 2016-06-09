8b25e5a22d18021d3d56b60287bdf2ec29cd3bdb
APerezG95 <adr.perez95@gmail.com>
2016-06-09 12:59:10 +0200

    Update CVoley.cpp

--- videojueguinchiproject/CVoley.cpp
+++ videojueguinchiproject/CVoley.cpp
@@ -1,9 +1,23 @@
 #include "CVoley.h"
 
-
+void CVoley::Inicializa()
+{
+	m_bObjOn = false;
+	m_iAtq_fis = 40;					//Escala sobre 100
+	m_iAtq_hab = 0;
+	m_iSaludMax = m_iSalud = 750;		//Escala sobre 1000
+	m_iDef_fis = 0.3f;
+	m_iDef_hab = 0.5f;
+	m_iVel = 4;							//Escala sobre 20
+	m_iBando = true;
+	m_iNivel = 1;
+	m_iAguante = m_iAguanteMax = 100;	//Escala sobre 500
+	m_bDisp = true;
+}
 
 CVoley::CVoley()
 {
+	CVoley::Inicializa();
 }
 
 
