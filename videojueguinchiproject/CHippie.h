945d25a41c52fee91b6bb471a0a21c2a351cc4ec
APerezG95 <adr.perez95@gmail.com>
2016-06-11 11:56:18 +0200

    Update CHippie.h

--- videojueguinchiproject/CHippie.h
+++ videojueguinchiproject/CHippie.h
@@ -7,4 +7,5 @@ public:
 	virtual void Inicializa();
 	CHippie();
 	~CHippie();
+		virtual bool ataque_esp(CPersonaje &p, int consumo = 0, bool tipodaño = 0, int daño_base = 1);	//Tipodaño 0 para curar y 1 para hacer daño de habilidad. daño_base es el daño de la habilidad. Consumo es para el gasto de Aguante.
 };
