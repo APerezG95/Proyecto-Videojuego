ebaef19a52de89a2d723230f131f90ae7fa6c978
APerezG95 <adr.perez95@gmail.com>
2016-06-11 12:54:51 +0200

    Update CEmpollon.cpp

--- videojueguinchiproject/CEmpollon.cpp
+++ videojueguinchiproject/CEmpollon.cpp
@@ -5,19 +5,19 @@ bool CEmpollon::ataque_esp(CPersonaje &p, int consumo, bool tipodaño, int daño
 	float danio;
 	if (tipodaño == 0) {					//Buffa al aliado
 		if (m_iAguante < consumo) return 0;
-		p->setm_iAtq_hab(p->getm_iAtq_hab()+daño_base);
-		p->setm_iAtq_fis(p->getm_iAtq_fis()+daño_base);
+		p.setm_iAtq_hab(p.getm_iAtq_hab()+daño_base);
+		p.setm_iAtq_fis(p.getm_iAtq_fis()+daño_base);
 		m_iAguante -= consumo;
 		return 1;
 	}
 	else                                       //daño fisico
 	{
 		if (m_iAguante < consumo) return 0;
-		danio = daño_base*(1 - p->getm_iDef_fis());
-		p->setm_iSalud(p->getm_iSalud()-danio);
-		if (p->getm_iSalud() <= 0) {
-			p->setm_iSalud(0);
-			p->setm_bDisp(false);
+		danio = daño_base*(1 - p.getm_iDef_fis());
+		p.setm_iSalud(p.getm_iSalud()-danio);
+		if (p.getm_iSalud() <= 0) {
+			p.setm_iSalud(0);
+			p.setm_bDisp(false);
 		}
 		m_iAguante -= consumo;
 		return 1;
