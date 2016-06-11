61dea71b3bff358e8b017b5e4877df0ba9ec357c
APerezG95 <adr.perez95@gmail.com>
2016-06-11 12:48:45 +0200

    Update CDestruye.cpp

--- videojueguinchiproject/CDestruye.cpp
+++ videojueguinchiproject/CDestruye.cpp
@@ -5,10 +5,10 @@ bool CDestruye::ataque_esp(CPersonaje &p, int consumo, bool tipodaño, int daño
 	float danio;
 	if (tipodaño == 0) {					//Debilita al enemigo
 		if (m_iAguante < consumo) return 0;
-		setm_iAtq_fis(0.4*getm_iAtq_fis());
-		setm_iAtq_hab(0.4*getm_iAtq_hab());
-		setm_iDef_fis(0.5*getm_iDef_fis());
-		setm_iDef_hab(0.5*getm_iDef_hab());
+		p->setm_iAtq_fis(0.4*getm_iAtq_fis());
+		p->setm_iAtq_hab(0.4*getm_iAtq_hab());
+		p->setm_iDef_fis(0.5*getm_iDef_fis());
+		p->setm_iDef_hab(0.5*getm_iDef_hab());
 		m_iAguante -= consumo;
 		return 1;
 		
@@ -16,11 +16,11 @@ bool CDestruye::ataque_esp(CPersonaje &p, int consumo, bool tipodaño, int daño
 	else                                       //daño habilidad
 	{
 		if (m_iAguante < consumo) return 0;
-		danio = daño_base*(1 - getm_iDef_hab());
-		setm_iSalud(getm_iSalud()-danio;)
-		if (getm_iSalud() <= 0) {
-			setm_iSalud(0);
-			setm_bDisp(false);
+		danio = daño_base*(1 - p->getm_iDef_hab());
+		p->setm_iSalud(p->getm_iSalud()-danio;)
+		if (p->getm_iSalud() <= 0) {
+			p->setm_iSalud(0);
+			p->setm_bDisp(false);
 		}
 		m_iAguante -= consumo;
 		return 1;
