e6bdd786fb87893b4b44531f1487b1dc1c8c1190
APerezG95 <adr.perez95@gmail.com>
2016-06-11 12:00:35 +0200

    Update CEmpollon.h

--- videojueguinchiproject/CEmpollon.h
+++ videojueguinchiproject/CEmpollon.h
@@ -7,5 +7,6 @@ public:
 virtual void Inicializa();
 	CEmpollon();
 	~CEmpollon();
+		virtual bool ataque_esp(CPersonaje &p, int consumo = 0, bool tipodaño = 0, int daño_base = 1);	//Tipodaño 0 para buffar y 1 para hacer daño de habilidad. daño_base es el daño de la habilidad. Consumo es para el gasto de Aguante.
 };
 
