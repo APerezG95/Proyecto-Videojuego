4e0365d68110b453fd97a6df97f55f6552f8789f
APerezG95 <adr.perez95@gmail.com>
2016-06-12 11:27:55 +0200

    Update CJefe.cpp

--- videojueguinchiproject/CJefe.cpp
+++ videojueguinchiproject/CJefe.cpp
@@ -1,5 +1,31 @@
 #include "CJefe.h"
 
+void CJefe::dibuja(int i, int j) {
+	glEnable(GL_TEXTURE_2D);
+
+	glBindTexture(GL_TEXTURE_2D, ETSIDI::getTexture("imagenes/fondo.png").id);
+
+	glDisable(GL_LIGHTING);
+
+	glBegin(GL_POLYGON);
+
+	glColor3f(1, 1, 1);
+
+	glTexCoord2d(0, 1); glVertex3f(-10, 0, -0.1);
+
+	glTexCoord2d(1, 1); glVertex3f(10, 0, -0.1);
+
+	glTexCoord2d(1, 0); glVertex3f(10, 15, -0.1);
+
+	glTexCoord2d(0, 0); glVertex3f(-10, 15, -0.1);
+
+	glEnd();
+
+	glEnable(GL_LIGHTING);
+
+	glDisable(GL_TEXTURE_2D);
+}
+
 
 
 CJefe::CJefe(int a, int b, int c, float d, float e, int f, int g)
