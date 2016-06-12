109cb2b3760773ba987be424d62d8c7aa43e18a2
APerezG95 <adr.perez95@gmail.com>
2016-06-12 11:21:48 +0200

    Update CBasket.cpp

--- videojueguinchiproject/CBasket.cpp
+++ videojueguinchiproject/CBasket.cpp
@@ -14,7 +14,31 @@
 
 #include "CBasket.h"
 
+void CBasket::dibuja(int i, int j) {
+	glEnable(GL_TEXTURE_2D);
 
+	glBindTexture(GL_TEXTURE_2D, ETSIDI::getTexture("imagenes/fondo.png").id);
+
+	glDisable(GL_LIGHTING);
+
+	glBegin(GL_POLYGON);
+
+	glColor3f(1, 1, 1);
+
+	glTexCoord2d(0, 1); glVertex3f(-10, 0, -0.1);
+
+	glTexCoord2d(1, 1); glVertex3f(10, 0, -0.1);
+
+	glTexCoord2d(1, 0); glVertex3f(10, 15, -0.1);
+
+	glTexCoord2d(0, 0); glVertex3f(-10, 15, -0.1);
+
+	glEnd();
+
+	glEnable(GL_LIGHTING);
+
+	glDisable(GL_TEXTURE_2D);
+}
 
 void CBasket::Inicializa()
 {
