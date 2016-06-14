90c9076aa6c4e8de641e77b2f0b1b02ff0b2a166
APerezG95 <adr.perez95@gmail.com>
2016-06-14 22:26:34 +0200

    Update CEnemigo.cpp

--- videojueguinchiproject/CEnemigo.cpp
+++ videojueguinchiproject/CEnemigo.cpp
@@ -30,27 +30,23 @@ void CEnemigo::dibuja(int i, int j) {
 	glDisable(GL_TEXTURE_2D);
 }
 
-void CEnemigo::Inicializa(int a, int b, int c, float d, float e, int f, int g)
+
+CEnemigo::~CEnemigo()
 {
-	m_iAtq_fis = a;
-	m_iAtq_hab = b;
-	m_iSaludMax = m_iSalud = c;
-	m_iDef_fis = d;
-	m_iDef_hab = e;
+}
+
+CEnemigo::~CEnemigo()
+{
+	m_iAtq_fis = 25;
+	m_iAtq_hab = 25;
+	m_iSaludMax = m_iSalud = 1000;
+	m_iDef_fis = 0,4;
+	m_iDef_hab = 0,4;
 	m_iVel = 3;				//constante en todos los enemigos
 	m_iBando = false;
-	m_iNivel = f;
-	m_iAguante = m_iAguanteMax = g;
+	m_iNivel = 1;
+	m_iAguante = m_iAguanteMax = 200;
 	m_bDisp = true;
-}
-
-
-CEnemigo::~CEnemigo()
-{
-}
-
-CEnemigo::~CEnemigo()
-{
 }
 
 void CEnemigo::SeleccionObjetivo(CMapa map) {
