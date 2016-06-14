1f4e32957bf4d06d27766e330f49a49ba50831cb
APerezG95 <adr.perez95@gmail.com>
2016-06-14 22:17:00 +0200

    Update CEnemigo.h

--- videojueguinchiproject/CEnemigo.h
+++ videojueguinchiproject/CEnemigo.h
@@ -5,9 +5,9 @@ class CEnemigo :
 {
 public:
 virtual void dibuja(int, int);
-virtual void Inicializa(); //No la usamos
-	CEnemigo(int a, int b, int c, float d, float e, int f, int g); //ataque fisico, habilidad, salud, def fis, def hab, nivel, aguante
+    void Inicializa(int a, int b, int c, float d, float e, int f, int g); //ataque fisico, habilidad, salud, def fis, def hab, nivel, aguante
+	CEnemigo(); 
 	~CEnemigo();
-	void SeleccionObjetivo(CMapa map);	//Ya ataca en esta funcion
+	void SeleccionObjetivo(CMapa map);
 };
 
