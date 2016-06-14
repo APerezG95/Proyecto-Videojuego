ef65a1800bb109ff895b2253244a2babb8d2161e
APerezG95 <adr.perez95@gmail.com>
2016-06-14 22:03:46 +0200

    Update CEnemigo.h

--- videojueguinchiproject/CEnemigo.h
+++ videojueguinchiproject/CEnemigo.h
@@ -8,5 +8,6 @@ virtual void dibuja(int, int);
 virtual void Inicializa(); //No la usamos
 	CEnemigo(int a, int b, int c, float d, float e, int f, int g); //ataque fisico, habilidad, salud, def fis, def hab, nivel, aguante
 	~CEnemigo();
+	void SeleccionObjetivo(CMapa map);
 };
 
